d771234bef8e14cea75acffcad8fe0e153e0f434
june-777 <wlwhswnsrl96@gmail.com>
2023-12-01 00:40:41 +0900

    feat(Application): 프로그램 시작 로직 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,11 +1,19 @@
 package subway;
 
 import java.util.Scanner;
+import subway.configuration.ApplicationConfiguration;
+import subway.controller.Controller;
+import subway.controller.InitController;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InitController initController = new InitController();
+        initController.process();
 
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+        Controller controller = applicationConfiguration.controller(scanner);
+
+        controller.process();
     }
 }
